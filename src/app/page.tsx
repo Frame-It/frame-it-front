f272977c067a183e9cb22c539a9fb55ea97aee23
OHS <ohs6006@gmail.com>
2024-07-22 06:20:47 +0900

    init : 마스터 브랜치 UI 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ const carouselArray = [
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-y-8 p-24">
-      <h1>Github Action 테스트</h1>
+      <h1>Github Action 마스터 브랜치 입니다!</h1>
       <div className="before:bg-gradient-radial after:bg-gradient-conic relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
