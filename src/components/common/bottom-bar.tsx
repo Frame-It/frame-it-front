11b1febaa5660470cbdf39d4fcc05b367ca3ef4e
OHS <ohs6006@gmail.com>
2024-08-13 06:02:24 +0900

    init : feed 경로 설정 및 레이아웃 설정
    - esLint 오류를 수정하였습니다.
    - feed 페이지 경로를 설정하였습니다.
    - 전체 레이아웃 최대 너비를 640px로 설정 하였습니다.

---
+++ src/components/common/bottom-bar.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+interface IBottomBarProps {}
+
+const BottomBar: React.FunctionComponent<IBottomBarProps> = () => {
+  return <nav></nav>;
+};
+
+export default BottomBar;
