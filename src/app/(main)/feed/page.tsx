acf68f761a4171d21066515e2b9dc69578a4847b
OHS <ohs6006@gmail.com>
2024-08-31 20:18:20 +0900

    feat : feed 페이지 필터 적용

--- src/app/(main)/feed/page.tsx
+++ src/app/(main)/feed/page.tsx
@@ -1,8 +1,31 @@
+'use client';
+
+import { FilterTabs } from '@/components/common/filter-tabs';
 import FeedList from '@/components/feed/feed-list';
+import { USER_TYPE, UserValue } from '@/types/filter';
+
+interface ITabData {
+  value: UserValue;
+  label: string;
+}
+
+const tabsData: ITabData[] = [
+  { value: 'ALL', label: `${USER_TYPE.ALL}` },
+  { value: 'MODEL', label: `${USER_TYPE.MODEL}` },
+  { value: 'PHOTOGRAPHER', label: `${USER_TYPE.PHOTOGRAPHER}` },
+];
 
 export default function FeedPage() {
+  const handleChange = (value: string) => {
+    console.log('handleChange', value);
+  };
   return (
     <div className="px-[16px] py-[1px]">
+      <FilterTabs
+        defaultValue="ALL"
+        onValueChange={handleChange}
+        tabsData={tabsData}
+      />
       <FeedList />
     </div>
   );
