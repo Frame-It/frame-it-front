28bd1c176557cc6d932adb2c49be4a336891b842
OHS <ohs6006@gmail.com>
2024-09-06 16:44:27 +0900

    feat : 북마크 페이지 UI

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import BottomNavbar from '@/components/common/bottom-navbar';
 import { Header, HeaderLeft, HeaderRight } from '@/components/common/header';
 import Icon from '@/components/common/icon';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Layout({
   children,
@@ -12,15 +13,17 @@ export default function Layout({
     <div className="pb-[66px] pt-[58px]">
       <Header>
         <HeaderLeft>
-          <div className="relative h-[23px] w-[65px]">
-            <Image
-              src="/logo.png"
-              alt="로고"
-              priority
-              fill
-              className="object-contain"
-            />
-          </div>
+          <Link href="/">
+            <div className="relative h-[23px] w-[65px]">
+              <Image
+                src="/logo.png"
+                alt="로고"
+                priority
+                fill
+                className="object-contain"
+              />
+            </div>
+          </Link>
         </HeaderLeft>
         <HeaderRight>
           <Icon id="notification-icon" className="h-6 w-6" />
