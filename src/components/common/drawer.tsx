3ea911fbb5acc2207b781ed781a16de461021c57
callmejeje <choije401@naver.com>
2024-09-07 15:59:22 +0900

    feat: children padding 수정

--- src/components/common/drawer.tsx
+++ src/components/common/drawer.tsx
@@ -49,7 +49,7 @@ const Drawer = ({
             )}
           </DrawerHeader>
         )}
-        <div className="p-4">{children}</div>
+        <div className="px-4 pb-4">{children}</div>
       </DrawerContent>
     </UiDrawer>
   );
