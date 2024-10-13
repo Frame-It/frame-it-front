4e98aa7db48b813f69012d5ebba84ff425c599ae
callmejeje <choije401@naver.com>
2024-10-13 21:49:12 +0900

    feat: 프로젝트 시작하기 모달 적용

--- src/components/project/apply-member-item.tsx
+++ src/components/project/apply-member-item.tsx
@@ -9,6 +9,7 @@ import { cn } from '@/lib/utils';
 import { ActiveStatus } from '@/types/project.type';
 import { useRouter, useSearchParams } from 'next/navigation';
 import BottomButton from '../common/bottom-button';
+import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog';
 import ReviewDialog from './review/review-dialog';
 
 interface ApplyMemberItemLayoutProps {
@@ -141,12 +142,19 @@ const RecruitingButtons = ({
 }: RecruitingButtonsProps) => {
   const router = useRouter();
   const searchParams = useSearchParams();
+  const {
+    isOpen: isModalOpen,
+    onOpenChange: onModalOpenChange,
+    onClose: onModalClose,
+    onOpen: onModalOpen,
+  } = useDisclosure(false);
+
   const handleClickStart = async () => {
     try {
       await postStartProject(projectId, applicantId);
       const updatedSearchParams = new URLSearchParams(searchParams.toString());
       updatedSearchParams.set('status', 'IN_PROGRESS');
-
+      onModalClose();
       router.replace(`?${updatedSearchParams.toString()}`);
     } catch (e) {
       alert(e);
@@ -162,13 +170,41 @@ const RecruitingButtons = ({
         label={'DM'}
         className="font-tag-12 max-w-none flex-1"
       />
-      <BottomButton
-        variant="secondary"
-        size="small"
-        label={'프로젝트 시작하기'}
-        className="font-tag-12 max-w-none flex-1"
-        onClick={handleClickStart}
-      />
+      <Dialog open={isModalOpen} onOpenChange={onModalOpenChange}>
+        <DialogTrigger asChild>
+          <BottomButton
+            variant="secondary"
+            size="small"
+            label={'프로젝트 시작하기'}
+            className="font-tag-12 max-w-none flex-1"
+            onClick={onModalOpen}
+          />
+        </DialogTrigger>
+        <DialogContent className="flex w-[312px] flex-col gap-[6px] px-[12px] pb-[24px] pt-[32px]">
+          <div
+            className={cn(
+              'font-title-16 flex flex-wrap items-start justify-center self-stretch',
+            )}
+          >
+            {'프로젝트를\u00A0'}
+            <span className="text-primary">시작</span>하시나요?
+          </div>
+          <p
+            className={cn(
+              'font-body-14 mb-4 flex justify-center self-stretch text-gray-20',
+            )}
+          >
+            열린 프로젝트는 변경할 수 없습니다.
+          </p>
+          <BottomButton
+            variant={'primary'}
+            size={'middle'}
+            label={'시작하기'}
+            onClick={handleClickStart}
+            className="max-w-none"
+          />
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
@@ -196,6 +232,7 @@ interface MyApplyButtonProps {
 
 const MyApplyButton = ({ projectId }: MyApplyButtonProps) => {
   // TODO: 신청 취소
+  const handleCancelApply = () => {};
 
   return (
     <div className="flex gap-[6px]">
@@ -204,6 +241,7 @@ const MyApplyButton = ({ projectId }: MyApplyButtonProps) => {
         size="small"
         label={'신청 취소하기'}
         className="font-tag-12 max-w-none flex-1"
+        onClick={handleCancelApply}
       />
     </div>
   );
