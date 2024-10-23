f1c0029180c3a87912034bfbd35ece5e0c74b115
callmejeje <choije401@naver.com>
2024-10-23 13:42:16 +0900

    fix: 프로젝트 등록 글자수 제한

--- src/components/project/write/step-two.tsx
+++ src/components/project/write/step-two.tsx
@@ -84,7 +84,6 @@ const StepTwo = ({ isEdit = false }: { isEdit?: boolean }) => {
       try {
         await postAnnouncement(formData);
         router.push(`?complete=true&title=${projectInfo.projectName}`);
-
         reset();
       } catch (error) {
         console.error(error);
@@ -128,6 +127,7 @@ const StepTwo = ({ isEdit = false }: { isEdit?: boolean }) => {
             className={cn('resize-none border border-gray-60 p-2')}
             placeholder="ex) 자연광 스튜디오에서 함께 촬영하실 모델분을 찾습니다!"
             rows={5}
+            maxLength={300}
           />
         </div>
 
@@ -139,6 +139,7 @@ const StepTwo = ({ isEdit = false }: { isEdit?: boolean }) => {
             className={cn('resize-none border border-gray-60 p-2')}
             placeholder="추가 수정 가능한 보정 횟수, 보정 스타일, 보정 툴 등을 적어주세요"
             rows={2}
+            maxLength={60}
           />
         </div>
       </div>
