953063cff0da24238e1441896a8b0b5eccbcca3a
callmejeje <choije401@naver.com>
2024-10-23 13:36:33 +0900

    fix: 마이 스튜디오 프로젝트 목록 공통 컴포넌트 사용

--- src/components/my-studio/project-list.tsx
+++ src/components/my-studio/project-list.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
-import { cn } from '@/lib/utils';
-import { useQuery } from '@tanstack/react-query';
 import { getMyProjects } from '@/service/client-actions/studio';
+import { useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import ProjectListItem from '../project/project-list-item';
 
 const ProjectList = () => {
   const router = useRouter();
@@ -36,46 +35,47 @@ const ProjectList = () => {
       ) : (
         <ul className="mt-[8px] flex flex-col gap-y-[18px]">
           {data?.projects.map((project: any, i: number) => (
-            <li
-              onClick={() => router.push('/')}
-              key={project.title + project.time + i}
-              className={cn(
-                'flex justify-between gap-x-[13px] pb-[18px]',
-                i === data?.projects.length - 1
-                  ? ''
-                  : 'border border-l-0 border-r-0 border-t-0 border-b-gray-80',
-              )}
-            >
-              <div className="space-y-[12px whitespace-nowrap">
-                <div className="font-semibold leading-[135%] text-gray-20">
-                  {project.title}
-                </div>
-                <div className="flex items-center leading-[150%] text-gray-40">
-                  <span className='after:mx-2 after:content-["|"]'>
-                    {project.location}
-                  </span>
-                  <span className='after:mx-2 after:content-["|"]'>
-                    {project.date}
-                  </span>
-                  <span>{project.time}</span>
-                </div>
-              </div>
-              <Badge
-                className={cn(
-                  'h-[22px] whitespace-nowrap bg-white px-[9px] text-xs leading-[150%] hover:bg-white',
-                  project.stste === 'RECRUITING' &&
-                    'border-sub-green text-sub-green',
-                  project.stste === 'IN_PROGRESS' &&
-                    'border-sub-blue text-sub-blue',
-                  project.stste === 'COMPLETED' &&
-                    'border-primary text-primary',
-                )}
-              >
-                {project.stste === 'RECRUITING' && <p>모집중</p>}
-                {project.stste === 'IN_PROGRESS' && '진행중'}
-                {project.stste === 'COMPLETED' && '완료'}
-              </Badge>
-            </li>
+            <ProjectListItem project={project} key={project.id} />
+            // <li
+            //   onClick={() => router.push('/')}
+            //   key={project.title + project.time + i}
+            //   className={cn(
+            //     'flex justify-between gap-x-[13px] pb-[18px]',
+            //     i === data?.projects.length - 1
+            //       ? ''
+            //       : 'border border-l-0 border-r-0 border-t-0 border-b-gray-80',
+            //   )}
+            // >
+            //   <div className="space-y-[12px whitespace-nowrap">
+            //     <div className="font-semibold leading-[135%] text-gray-20">
+            //       {project.title}
+            //     </div>
+            //     <div className="flex items-center leading-[150%] text-gray-40">
+            //       <span className='after:mx-2 after:content-["|"]'>
+            //         {project.location}
+            //       </span>
+            //       <span className='after:mx-2 after:content-["|"]'>
+            //         {project.date}
+            //       </span>
+            //       <span>{project.time}</span>
+            //     </div>
+            //   </div>
+            //   <Badge
+            //     className={cn(
+            //       'h-[22px] whitespace-nowrap bg-white px-[9px] text-xs leading-[150%] hover:bg-white',
+            //       project.stste === 'RECRUITING' &&
+            //         'border-sub-green text-sub-green',
+            //       project.stste === 'IN_PROGRESS' &&
+            //         'border-sub-blue text-sub-blue',
+            //       project.stste === 'COMPLETED' &&
+            //         'border-primary text-primary',
+            //     )}
+            //   >
+            //     {project.stste === 'RECRUITING' && <p>모집중</p>}
+            //     {project.stste === 'IN_PROGRESS' && '진행중'}
+            //     {project.stste === 'COMPLETED' && '완료'}
+            //   </Badge>
+            // </li>
           ))}
         </ul>
       )}
