e2fbd69fc4957b1e83d844755bdfb5679070c8e9
callmejeje <choije401@naver.com>
2024-10-23 15:25:17 +0900

    fix: 리뷰 작성하지 않고 뒤로가기 할 경우 프로젝트 상태에 맞는 관리 페이지로 이동

--- src/app/(project)/review-register/layout.tsx
+++ src/app/(project)/review-register/layout.tsx
@@ -3,20 +3,30 @@
 import BackButton from '@/components/common/back-button';
 import { Header, HeaderCenter, HeaderLeft } from '@/components/common/header';
 import Icon from '@/components/common/icon';
-import { useSearchParams } from 'next/navigation';
+import { useParams, useRouter, useSearchParams } from 'next/navigation';
 
 export default function ReviewRegisterLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const params = useParams();
+  const router = useRouter();
   const searchParams = useSearchParams();
   const isComplete = searchParams.get('complete') === 'true';
+  const isHost = searchParams.get('isHost');
+  const status = searchParams.get('status');
+
+  const handleClickBack = () => {
+    router.push(
+      `/project-management/${params.id}?status=${status}&isHost=${isHost}`,
+    );
+  };
   return (
     <div className="h-screen-dvh flex flex-col pt-[58px]">
       <Header>
         <HeaderLeft>
-          <BackButton>
+          <BackButton onClick={handleClickBack}>
             <Icon id="back-icon" className="size-[32px] text-gray-40" />
           </BackButton>
         </HeaderLeft>
