e0a2c4a3963e92fe2524cca2d2b5097f0acab6fe
OHS <ohs6006@gmail.com>
2024-10-28 07:46:39 +0900

    feat : 닉네임 수정 레이아웃 수정

--- src/app/(my)/my-page/nickname/page.tsx
+++ src/app/(my)/my-page/nickname/page.tsx
@@ -99,9 +99,9 @@ export default function ProfilePage() {
   const isDisabled = !isValid || isDuplicate || !checkDuplicate;
 
   return (
-    <main>
+    <main className="pt-[58px] xl:flex-1 xl:py-0">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="pt-[58px]">
+        <form onSubmit={form.handleSubmit(onSubmit)}>
           <Header>
             <HeaderLeft>
               <BackButton>
@@ -119,68 +119,70 @@ export default function ProfilePage() {
               </button>
             </HeaderRight>
           </Header>
-          <FormField
-            control={form.control}
-            name="nickname"
-            render={({ field, fieldState }) => {
-              const isError = fieldState.error;
-              const isValue = !!field.value;
+          <div className="px-4">
+            <FormField
+              control={form.control}
+              name="nickname"
+              render={({ field, fieldState }) => {
+                const isError = fieldState.error;
+                const isValue = !!field.value;
 
-              return (
-                <FormItem className="mt-[25px] p-1">
-                  <FormLabel>닉네임</FormLabel>
-                  <FormControl>
-                    <div className="flex items-center gap-x-[20px]">
-                      <Input
-                        placeholder="닉네임을 입력해 주세요."
-                        onChangeCapture={() => {
-                          setIsDuplicate(true);
-                          setCheckDuplicate(false);
-                          console.log('초기화');
-                        }}
-                        {...field}
-                        value={field.value || ''}
-                        className={cn(
-                          `rounded-none border border-l-0 border-r-0 border-t-0 border-gray-10 focus-visible:ring-0 focus-visible:ring-offset-0`,
-                          fieldState.error && 'border-error',
-                        )}
-                      />
-                      <Button
-                        type="button"
-                        disabled={!field.value}
-                        size="sm"
-                        className="px-[12px] py-[8px] text-white disabled:bg-gray-80 disabled:text-gray-20"
-                        onClick={() => handleEmailCheck(field.value)}
-                      >
-                        중복 확인
-                      </Button>
-                    </div>
-                  </FormControl>
-                  {!isError && !isValue && (
-                    <FormDescription>
-                      한글 or 영어로 2~8글자 내로 가능합니다.
-                    </FormDescription>
-                  )}
-                  {!isError && isValue && checkDuplicate && !isDuplicate ? (
-                    <FormDescription className="text-sub-green">
-                      사용할 수 있는 닉네임 입니다.
-                    </FormDescription>
-                  ) : null}
-                  {!isError && isValue && checkDuplicate && isDuplicate ? (
-                    <FormDescription className="text-error">
-                      중복된 닉네임 입니다.
-                    </FormDescription>
-                  ) : null}
-                  {!fieldState.error && !!field.value && !checkDuplicate && (
-                    <FormDescription className="text-error">
-                      닉네임 중복 확인을 해주세요.
-                    </FormDescription>
-                  )}
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
+                return (
+                  <FormItem className="mt-[25px] p-1">
+                    <FormLabel>닉네임</FormLabel>
+                    <FormControl>
+                      <div className="flex items-center gap-x-[20px]">
+                        <Input
+                          placeholder="닉네임을 입력해 주세요."
+                          onChangeCapture={() => {
+                            setIsDuplicate(true);
+                            setCheckDuplicate(false);
+                            console.log('초기화');
+                          }}
+                          {...field}
+                          value={field.value || ''}
+                          className={cn(
+                            `rounded-none border border-l-0 border-r-0 border-t-0 border-gray-10 focus-visible:ring-0 focus-visible:ring-offset-0`,
+                            fieldState.error && 'border-error',
+                          )}
+                        />
+                        <Button
+                          type="button"
+                          disabled={!field.value}
+                          size="sm"
+                          className="px-[12px] py-[8px] text-white disabled:bg-gray-80 disabled:text-gray-20"
+                          onClick={() => handleEmailCheck(field.value)}
+                        >
+                          중복 확인
+                        </Button>
+                      </div>
+                    </FormControl>
+                    {!isError && !isValue && (
+                      <FormDescription>
+                        한글 or 영어로 2~8글자 내로 가능합니다.
+                      </FormDescription>
+                    )}
+                    {!isError && isValue && checkDuplicate && !isDuplicate ? (
+                      <FormDescription className="text-sub-green">
+                        사용할 수 있는 닉네임 입니다.
+                      </FormDescription>
+                    ) : null}
+                    {!isError && isValue && checkDuplicate && isDuplicate ? (
+                      <FormDescription className="text-error">
+                        중복된 닉네임 입니다.
+                      </FormDescription>
+                    ) : null}
+                    {!fieldState.error && !!field.value && !checkDuplicate && (
+                      <FormDescription className="text-error">
+                        닉네임 중복 확인을 해주세요.
+                      </FormDescription>
+                    )}
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+          </div>
         </form>
       </Form>
     </main>
