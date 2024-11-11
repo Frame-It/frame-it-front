dc248e3c93ae083554edac90aa91a6aad12ee975
callmejeje <choije401@naver.com>
2024-11-11 16:21:03 +0900

    feat: 프로젝트 공고 empty 화면 추가

--- src/app/(main)/recruit/page.tsx
+++ src/app/(main)/recruit/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import BottomButton from '@/components/common/bottom-button';
 import { FilterTabs } from '@/components/common/filter-tabs';
-import LoadingSpinner from '@/components/common/loading-spinner';
+import Icon from '@/components/common/icon';
 import RecruitCard from '@/components/project/recruit-card';
 import FilterDrawers from '@/components/project/recruitment/filter-drawers';
 import { ScrollArea } from '@/components/ui/scroll-area';
@@ -15,6 +16,7 @@ import {
   LocationType,
   TimeOption,
 } from '@/types/project.type';
+import { useRouter } from 'next/navigation';
 
 const RECRUIT = '구인';
 
@@ -49,6 +51,7 @@ interface RecruitPageProps {
 }
 
 const RecruitPage = ({ searchParams }: RecruitPageProps) => {
+  const router = useRouter();
   const filter: IRecruitFilter = {
     recruitmentRole:
       searchParams.tab === 'all' ? undefined : (searchParams.tab as Identity),
@@ -72,25 +75,43 @@ const RecruitPage = ({ searchParams }: RecruitPageProps) => {
   }
 
   return (
-    <div className={cn('relative h-full overflow-hidden')}>
+    <div className={cn('relative flex h-full flex-col overflow-hidden')}>
       <div className={cn('sticky z-10 bg-white')}>
         <FilterTabs tabsData={tabsData} currentTab={currentTab} />
         <div className={cn('h-[46px]')}>
           <FilterDrawers filter={filter} />
         </div>
       </div>
-      <ScrollArea
-        className={cn(
-          'h-[calc(100%-94px)] overflow-auto py-[19px] xl:h-[calc(800px-94px-24px-120px)]',
-        )}
-      >
-        <div className={cn('flex flex-col gap-[16px] px-[16px]')}>
-          {isLoading && <div>Loading...</div>}
-          {recruitList?.map((recruit: IRecruitProject) => (
-            <RecruitCard key={recruit.id} {...recruit} />
-          ))}
+      {recruitList?.length === 0 ? (
+        <div className="flex flex-1 flex-col items-center justify-center gap-[9px] text-center">
+          <Icon id={'pictogram-icon'} width={87} height={71} stroke="#7E7774" />
+          <div>
+            <div className="font-title-16 whitespace-pre-line text-gray-20">
+              {'나의 작업메이트를\n찾아볼까요?'}
+            </div>
+          </div>
+          <BottomButton
+            variant={'secondary'}
+            size={'middle'}
+            label={'프로젝트 등록 하기'}
+            className="w-fit px-6"
+            onClick={() => router.push('/project-register')}
+          />
         </div>
-      </ScrollArea>
+      ) : (
+        <ScrollArea
+          className={cn(
+            'h-[calc(100%-94px)] overflow-auto py-[19px] xl:h-[calc(800px-94px-24px-120px)]',
+          )}
+        >
+          <div className={cn('flex h-full flex-col gap-[16px] px-[16px]')}>
+            {isLoading && <div>Loading...</div>}
+            {recruitList?.map((recruit: IRecruitProject) => (
+              <RecruitCard key={recruit.id} {...recruit} />
+            ))}
+          </div>
+        </ScrollArea>
+      )}
     </div>
   );
 };
