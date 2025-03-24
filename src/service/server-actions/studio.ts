67b7cf90d0f77107c774488db334f7f91c790b3f
callmejeje <choije401@naver.com>
2025-03-25 00:30:15 +0900

    feat: 토큰 없어도 포토폴리오 조회 가능하게 수정

--- src/service/server-actions/studio.ts
+++ src/service/server-actions/studio.ts
@@ -39,23 +39,23 @@ export const getGuestStudio = async (id?: string) => {
   const cookieStore = cookies();
   const token = cookieStore.get('accessToken');
 
-  if (token) {
-    const res = await fetch(`${API_URL}/users/${id}/studio`, {
-      method: 'GET',
-      cache: 'no-store',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token?.value}`,
-      },
-    });
+  // if (token) {
+  const res = await fetch(`${API_URL}/users/${id}/studio`, {
+    method: 'GET',
+    cache: 'no-store',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token?.value}`,
+    },
+  });
 
-    if (!res.ok) {
-      throw new Error('게스트 스튜디오 데이터 오류 : ' + res.status);
-    }
-
-    const data = await res.json();
-    return data;
+  if (!res.ok) {
+    throw new Error('게스트 스튜디오 데이터 오류 : ' + res.status);
   }
+
+  const data = await res.json();
+  return data;
+  // }
 
   return {};
 };
